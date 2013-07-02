e4e33ecbd38b6df4bae5a869abd30d749349b4ec
lewuathe <lewuathe@me.com>
2013-07-02 22:09:49 +0900

    Test in index.js

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ var apiClient         = require('./lib/apiClient.js');
 var Client = module.exports = function(config) {
 	var self    = this;
 	self.config = config;
-	
+	var statsListClient = new apiClient(config.type);
+	var params = { 
+		'appId' : '6d736004f09f38d8fc8bb827201c1ecf9b4cb84f',
+		'lang' : 'E',
+	};
+		
+	statsListClient.request(params, config.callback);
 	
 }
+
+
+function testCallBack(data){
+	console.log(data);
+}
+
+var config = {
+	type : "getStatsList",
+	callback : testCallBack
+};
+
+var c = new Client(config);
+
