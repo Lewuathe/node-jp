c7176fa9ac9d34639310e4ed472b7540a45d2ce0
Lewuathe <lewuathe@me.com>
2013-07-15 18:26:26 +0900

    test code

---
+++ test/list_test.js
@@ -0,0 +1,22 @@
+var nodeJP = require('../index.js');
+
+
+function testCallBack(data){
+	console.log(data);
+	console.log('------------------------');
+	console.log(data['GET_STATS_LIST']['DATALIST_INF']['LIST_INF']);
+}
+
+var config = {
+	appId : "6d736004f09f38d8fc8bb827201c1ecf9b4cb84f",
+};
+
+var params = { 
+	'lang' : 'E',
+	'surveyYears' : '198901-201201',
+	'searchWord' : 'kanagawa',
+	'searchKind' : 1
+};
+
+var c = new nodeJP(config);
+c.list(params, testCallBack);
