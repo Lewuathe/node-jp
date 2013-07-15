6db16477bb51cd2361e8bd7848d59190d3e3d362
Lewuathe <lewuathe@me.com>
2013-07-15 17:17:42 +0900

    make api

--- index.js
+++ index.js
@@ -11,21 +11,30 @@ var apiClient         = require('./lib/apiClient.js');
 var Client = module.exports = function(config) {
 	var self    = this;
 	self.config = config;
-	var statsListClient = new apiClient(config.type);
-	var params = { 
-		'appId' : '6d736004f09f38d8fc8bb827201c1ecf9b4cb84f',
-		'lang' : 'E',
-		'dataSetId' : 'kaisasak',
-		'statsDataId'  12,
-		'penSpecified' : 0,
-		'processMode' : 'E',
-		'dataSetName' : 'kaisasak_test',
-		'lvArea' : 1
-	};
-		
-	statsListClient.request(params, config.callback);
-	
+	self.appId  = config.appId;
 }
+
+Client.prototype.save = function(params, cb) {
+	var self = this;
+	var postDatasetClient = new apiClient('postDataset');
+	params.appId = self.appId;
+	postDatasetClient.request(params, cb);
+}
+
+Client.prototype.search = function(params, cb) {
+	var self = this;
+	var getStatsDataClient = new apiClient('getStatsData');
+	params.appId = self.appId;
+	getStatsDataClient.request(params, cb);
+}	
+
+Client.prototype.list = function(params, cb) {
+	var self = this;
+	var getStatsListClient = new apiClient('getStatsList');
+	params.appId = self.appId;
+	getStatsListClient.request(params, cb);
+}
+
 
 
 function testCallBack(data){
@@ -33,9 +42,20 @@ function testCallBack(data){
 }
 
 var config = {
-	type : "postDataset",
-	callback : testCallBack
+	appId : "6d736004f09f38d8fc8bb827201c1ecf9b4cb84f",
+};
+
+var params = { 
+	'appId' : '6d736004f09f38d8fc8bb827201c1ecf9b4cb84f',
+	'lang' : 'E',
+	'dataSetId' : 'kaisasak',
+	'statsDataId':  12,
+	'penSpecified' : 0,
+		'processMode' : 'E',
+	'dataSetName' : 'kaisasak_test',
+	'lvArea' : 1
 };
 
 var c = new Client(config);
+c.save(params, testCallBack);
 
