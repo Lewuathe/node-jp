7f9965215a5f681be664353a9c890adecc5d4d4a
Lewuathe <lewuathe@me.com>
2013-07-18 22:49:26 +0900

    Vows test code

--- test/list_test.js
+++ test/list_test.js
@@ -1,17 +1,22 @@
+var vows = require('vows');
+var assert = require('assert');
+
 var nodeJP = require('../index.js');
 var config = require('./test_config.json');
 
-
-function testCallBack(data){
-	console.log(data);
-}
-
-var params = { 
-	'lang' : 'E',
-	'surveyYears' : '198901-201201',
-	'searchWord' : 'kanagawa',
-	'searchKind' : 1
-};
-
-var c = new nodeJP(config);
-c.list(params, testCallBack);
+vows.describe('Data list test').addBatch({
+    'API access test' : {
+		topic : function() {
+			var params = { 
+				'lang' : 'E',
+				'surveyYears' : '201001-201201',
+				'searchKind' : 2
+			};
+			var c = new nodeJP(config);
+			c.list(params, this.callback);
+		},
+		'Can be accessed' : function(data) {
+			assert.isObject(data);
+		}
+	}		
+}).export(module);
--- test/meta_test.js
+++ test/meta_test.js
@@ -1,14 +1,22 @@
+var vows = require('vows');
+var assert = require('assert');
+
 var nodeJP = require('../index.js');
 var config = require('./test_config.json');
 
-function testCallBack(data){
-	console.log(data);
-}
+vows.describe('Meta information test').addBatch({
+    'API access test' : {
+		topic : function() {
+			var params = { 
+				'lang' : 'E',
+				'statsDataId' : '0003003333'
+			};
+			var c = new nodeJP(config);
+			c.meta(params, this.callback);
+		},
+		'Can be accessed' : function(data) {
+			assert.isObject(data);
+		}
+	}		
+}).export(module);
 
-var params = { 
-	'lang' : 'E',
-	'statsDataId' : '0003003333'
-};
-
-var c = new nodeJP(config);
-c.meta(params, testCallBack);
